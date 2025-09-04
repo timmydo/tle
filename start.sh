ab1ba8e567048968b0431349e1785db051ee374a
Timmy Douglas <mail@timmydouglas.com>
2025-09-03 22:09:18 -0700

    add --no-inform

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start TLE (Timmy's Lisp Environment)
 export CL_SOURCE_REGISTRY="$(pwd)//:"
-exec sbcl --no-userinit --non-interactive --eval '(sb-int:set-floating-point-modes :traps nil)' --eval "(require \"asdf\")" --eval '(asdf:load-system :tle)' --eval '(tle:main)'
+exec sbcl --noinform --no-userinit --non-interactive --eval '(sb-int:set-floating-point-modes :traps nil)' --eval "(require \"asdf\")" --eval '(asdf:load-system :tle)' --eval '(tle:main)'
